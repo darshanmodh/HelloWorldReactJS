1f43e6b1bb8a740a0ca0915e6333e3b800086516
dmodh <dmodh@yahoo-inc.com>
2017-02-09 15:17:24 -0800

    ReactJS form element

--- App.jsx
+++ App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: 'Initial data...'
+        }
+
+        this.updateState = this.updateState.bind(this);
+
+    };
+
+    updateState(e) {
+        this.setState({data: e.target.value});
+    }
+
     render() {
         return (
             <div>
-                Hello World!!!
+                <input type = "text" value = {this.state.data}
+                       onChange = {this.updateState} />
+                <h4>{this.state.data}</h4>
             </div>
         );
     }
