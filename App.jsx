a439d183ee0be7cc75171df7ea7423660e3a49d0
dmodh <dmodh@yahoo-inc.com>
2017-02-09 16:10:50 -0800

    ReactJS keys

--- App.jsx
+++ App.jsx
@@ -1,41 +1,52 @@
 import React from 'react';
 
 class App extends React.Component {
-
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
 
         this.state = {
-            data: 'Initial data...'
+            data:
+                [
+                    {
+                        component: 'First...',
+                        id: 1
+                    },
+
+                    {
+                        component: 'Second...',
+                        id: 2
+                    },
+
+                    {
+                        component: 'Third...',
+                        id: 3
+                    }
+                ]
         }
 
-        this.updateState = this.updateState.bind(this);
-    };
-
-    updateState(e) {
-        this.setState({data: e.target.value});
     }
 
     render() {
         return (
             <div>
-                <Content myDataProp = {this.state.data}
-                         updateStateProp = {this.updateState} />
+                <div>
+                    {this.state.data.map((dynamicComponent, i) => <Content
+                        key = {i} componentData = {dynamicComponent}/>)}
+                </div>
             </div>
         );
     }
 }
 
 class Content extends React.Component {
-
     render() {
         return (
             <div>
-                <input type = "text" value = {this.props.myDataProp}
-                       onChange = {this.props.updateStateProp} />
-                <h3>{this.props.myDataProp}</h3>
+                <div>{this.props.componentData.component}</div>
+                <div>{this.props.componentData.id}</div>
             </div>
         );
     }
 }
+
 export default App;
